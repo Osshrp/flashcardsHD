03b29295a928f47dddb32625607574875c9d62bc
Osshrp <osshrp@gmail.com>
2016-05-16 11:03:35 +0300

    Some minor fixing. Founded by Hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
-  def find_card id
+  def find_card(id)
     if id
       @card = current_user.cards.find id
-    else
-      if current_user.current_block
-        @card = current_user.current_block.cards.pending.first
-        @card ||= current_user.current_block.cards.repeating.first
+    elsif
+      current_user.current_block
+      @card = current_user.current_block.cards.pending.first
+      @card ||= current_user.current_block.cards.repeating.first
       else
         @card = current_user.cards.pending.first
         @card ||= current_user.cards.repeating.first
