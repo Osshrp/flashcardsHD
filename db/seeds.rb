72c335be19570e65c0fb27736333f06e2ee3c9bd
Osshrp <osshrp@gmail.com>
2016-05-27 07:36:35 +0300

    FIX seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,9 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open('https://www.learnathome.ru/blog/100-beautiful-words'))
 
-user = User.create(email: 'test@test.ru', password: '123',
-                   password_confirmation: '123')
+user = User.create(email: 'test@test.ru',
+                  password: '123',
+                  password_confirmation: '123')
 
 block = user.blocks.build(title: 'test')
 block.save
@@ -23,11 +24,12 @@ doc.xpath('//table/tbody/tr').each do |row|
   original = row.search('td[2]')[0].content.downcase
   translated = row.search('td[4]')[0].content.downcase
   card = Card.new(original_text: original,
-  	              translated_text: translated,
-                  user_id: user.id, block_id: block.id)
+  	             translated_text: translated,
+                 user_id: user.id, block_id: block.id)
   card.save
 end
 
-admin_user = User.create(email: 'admin@test.org', password: 'admin',
-                   password_confirmation: 'admin')
+admin_user = User.create(email: 'admin@test.org',
+                        password: 'admin',
+                        password_confirmation: 'admin')
 admin_user.add_role :admin
