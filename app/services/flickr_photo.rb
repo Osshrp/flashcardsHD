9ecac3d08d5232a5833b3f0a5cf061c2bcaf2339
Osshrp <osshrp@gmail.com>
2016-06-10 16:48:11 +0300

    lovely hound

--- app/services/flickr_photo.rb
+++ app/services/flickr_photo.rb
@@ -1,17 +1,16 @@
 class FlickrPhoto
+  class << self
+    def render(search_string, photo_count = 10)
+      photos(search_string, photo_count).in_groups_of(2)
 
-  def self.render(search_string, photo_count = 10)
-    photos(search_string, photo_count).in_groups_of(2)
-    
-    # rescue Exception
-    #   render :partial => '/cards/unavailable'
-  end
+      # rescue Exception
+      #   render :partial => '/cards/unavailable'
+    end
 
-  private
-
-  def self.photos(search_string, photo_count = 10)
-    init_hash = {:key => ENV['FLICKR_KEY'], :secret => ENV['FLICKR_SECRET']}
-    flickr = Flickr.new(init_hash)
-    flickr.photos.search(tags: search_string, per_page: photo_count)
+    def photos(search_string, photo_count = 10)
+      init_hash = { key: ENV['FLICKR_KEY'], secret: ENV['FLICKR_SECRET'] }
+      flickr = Flickr.new(init_hash)
+      flickr.photos.search(tags: search_string, per_page: photo_count)
+    end
   end
 end
