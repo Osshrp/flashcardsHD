6da527d11f543075449d7255b85485fbffdea536
Osshrp <osshrp@gmail.com>
2016-06-10 16:37:49 +0300

    ENV in test envioremnt

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module Flashcards
     config.i18n.default_locale = :ru
     config.i18n.available_locales = [:ru, :en]
     config.active_record.raise_in_transactional_callbacks = true
+
+    if Rails.env.test?
+      config do
+        env_file = File.join(Rails.root, 'config', 'local_env.yml')
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end if File.exists?(env_file)
+      end
+end
   end
 end
