d5f38e9ce1d07de5ab14d2609a19018f1770b18e
Osshrp <osshrp@gmail.com>
2016-06-12 09:56:12 +0300

    refactoring flickr rspec

--- spec/features/flickr_spec.rb
+++ spec/features/flickr_spec.rb
@@ -1,43 +1,29 @@
 require "rails_helper"
 require "support/helpers/login_helper.rb"
 include LoginHelper
-# require 'pry'
 require 'vcr'
-require 'pp'
 
-describe "seaching photos in Flickr", vcr: true do
+feature "User connects Flickr photos to card" do
   let!(:user) { create :user_with_one_block_and_one_card }
   before do
     visit root_path
     login "test@test.com", "12345", "Войти"
-    visit edit_card_path user.cards.first
   end
-
-  it "edit card searching and connecting photos from Flickr" do
+  scenario "they search photos in existing card" do
+    visit edit_card_path user.cards.first
     click_link 'Загрузить фото с Flickr'
     fill_in "card_photo_search_string", with: "house"
-
     VCR.use_cassette 'flickr', match_requests_on: [:host]  do
       click_button "Искать фото"
     end
     expect(page).to have_xpath "//li/a/img"
   end
-end
 
-describe "create card with Flickr photos connected" do
-  let!(:user) { create :user_with_one_block_without_cards }
-  before do
-    visit root_path
-    login "test@test.com", "12345", "Войти"
-    visit new_card_path
-  end
-
-  it "create card searching and connecting photos from Flickr" do
-    fill_in "card_original_text", with: "house"
-    fill_in "card_translated_text", with: "дом"
+  before { visit new_card_path }
+  
+  scenario "they search photos in new card page" do
     click_link 'Загрузить фото с Flickr'
     fill_in "card_photo_search_string", with: "house"
-
     VCR.use_cassette 'flickr', match_requests_on: [:host] do
       click_button "Искать фото"
     end
