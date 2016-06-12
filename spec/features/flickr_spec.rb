ed4868e66c9ff9c00e6b36edcd9cdb3a1b8736ad
Osshrp <osshrp@gmail.com>
2016-06-12 10:01:53 +0300

    Lovely hound

--- spec/features/flickr_spec.rb
+++ spec/features/flickr_spec.rb
@@ -13,14 +13,14 @@ feature "User connects Flickr photos to card" do
     visit edit_card_path user.cards.first
     click_link 'Загрузить фото с Flickr'
     fill_in "card_photo_search_string", with: "house"
-    VCR.use_cassette 'flickr', match_requests_on: [:host]  do
+    VCR.use_cassette 'flickr', match_requests_on: [:host] do
       click_button "Искать фото"
     end
     expect(page).to have_xpath "//li/a/img"
   end
 
   before { visit new_card_path }
-  
+
   scenario "they search photos in new card page" do
     click_link 'Загрузить фото с Flickr'
     fill_in "card_photo_search_string", with: "house"
